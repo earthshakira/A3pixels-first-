b30856b5992629c7699a46397be2ce2ad8b4dc51
earthshakira <arawkar.shubham08@gmail.com>
2015-12-02 18:55:57 +0530

    second time

---
+++ Page/dist/js/custom.js
@@ -0,0 +1,14 @@
+var click=false;
+$('.subhead').on('click',function(){
+  if(click){
+      $('#explanation').css('margin-top','-70vh');
+      $('#explanation').css('opacity','1');
+      $('.subhead-text').text('Explanation');
+  }
+  else{
+    $('#explanation').css('margin-top','-140vh');
+    $('#explanation').css('opacity','0');
+    $('.subhead-text').text('Question');
+  }
+    click=!click;
+});
